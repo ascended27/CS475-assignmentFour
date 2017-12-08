d85c7c997212527afebe57a58eb9c884e183441f
ascended27 <ascended27@gmail.com>
2017-12-07 19:17:30 -0500

    Comments

--- AssignmentFour-Client/src/rmiproject/ui/AlertBox.java
+++ AssignmentFour-Client/src/rmiproject/ui/AlertBox.java
@@ -13,31 +13,39 @@ import javafx.stage.Stage;
 public class AlertBox {
 
     public static void display(String title, String message, boolean fatal) {
+        // Create a new stage for the alert
         Stage window = new Stage();
 
+        // Set some attributes about the window
         window.initModality(Modality.APPLICATION_MODAL);
         window.setTitle(title);
         window.setMinWidth(250);
 
+        // Set the label and message
         Label label = new Label();
         label.setFont(Font.font("GT Walsheim", 14));
         Label mytitle = new Label();
         mytitle.setFont(Font.font("GT Walsheim", 18));
-
         mytitle.setText(title+"\n\n");
         label.setText(message);
 
 
+        // Notify the user of fatal error
         if(fatal)
         {
             label.setText(message+"\nThis is a FATAL error. The program will exit once you hit Close.");
         }
 
+        // Setup the close button
         Button closeButton = new Button("Close");
         closeButton.setOnAction(e -> {
-            window.close();
+            if(fatal)
+                System.exit(1);
+            else
+                window.close();
         });
 
+        // Position all the elements in the alert
         VBox layout = new VBox(20);
         VBox buttonBox = new VBox();
         buttonBox.setPadding(new Insets(10, 20, 10, 0));
@@ -46,19 +54,16 @@ public class AlertBox {
         layout.setPrefWidth(600);
         VBox content = new VBox();
         content.setStyle("-fx-background-color: #f3f3f3");
-//        content.setAlignment(Pos.CENTER);
         content.setPadding(new Insets(20, 20, 20, 20));
         layout.setStyle("-fx-background-color: white");
         content.getChildren().addAll(mytitle, label);
         layout.getChildren().addAll(content, buttonBox);
         layout.setAlignment(Pos.CENTER);
 
+        // Display the alert
         Scene scene = new Scene(layout);
         window.setScene(scene);
         window.showAndWait();
-
-        if(fatal)
-            System.exit(1);
     }
 
 }
